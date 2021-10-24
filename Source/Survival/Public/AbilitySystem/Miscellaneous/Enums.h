c594dc2bf9192d4c216e915a6f3bc13d102b850d
Gradess <trofimovjava@gmail.com>
2021-10-25 00:59:00 +0300

    Add movement state enum

---
+++ Source/Survival/Public/AbilitySystem/Miscellaneous/Enums.h
@@ -0,0 +1,19 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enums.generated.h"
+
+UENUM(
+	BlueprintType,
+	meta = (
+		BitFlags
+	)
+)
+enum class EMovementState : uint8
+{
+	Idle,
+	Walk,
+	Run
+};
