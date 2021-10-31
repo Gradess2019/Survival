203551fc822cfbc22405891ee523827a33b7f022
Gradess <trofimovjava@gmail.com>
2021-10-31 21:02:50 +0300

    Add simple grid cell

---
+++ Source/Survival/Private/Core/WorldGrid/GridCell.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "Core/WorldGrid/GridCell.h"
+
+
+AGridCell::AGridCell()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+AActor* AGridCell::GetEdgeActor(EGridDirection Direction)
+{
+	const auto FoundActor = EdgeActors.Find(Direction);
+	return FoundActor != nullptr ? *FoundActor : nullptr;
+}
+
+bool AGridCell::SetEdgeActor(AActor* NewActor, EGridDirection Direction)
+{
+	return IsValid(EdgeActors.Add(Direction, NewActor));
+}
+
+TMap<EGridDirection, AActor*> AGridCell::GetEdgeActors() const
+{
+	return EdgeActors;
+}
+
---
+++ Source/Survival/Public/Core/WorldGrid/GridCell.h
@@ -0,0 +1,45 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GridDirectionEnum.h"
+#include "GameFramework/Actor.h"
+#include "GridCell.generated.h"
+
+UCLASS(
+	BlueprintType,
+	Blueprintable
+)
+class SURVIVAL_API AGridCell : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AGridCell();
+
+	UFUNCTION(
+		BlueprintPure,
+		Category = "GridCell"
+	)
+	AActor* GetEdgeActor(EGridDirection Direction);
+
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "GridCell"
+	)
+	bool SetEdgeActor(AActor* NewActor, EGridDirection Direction);
+
+	UFUNCTION(
+		BlueprintPure,
+		BlueprintGetter,
+		Category = "GridCell"
+	)
+	TMap<EGridDirection, AActor*> GetEdgeActors() const;
+
+protected:
+	UPROPERTY(
+		BlueprintGetter = "GetEdgeActors"
+	)
+	TMap<EGridDirection, AActor*> EdgeActors;
+};
