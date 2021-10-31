f24c6d772baf697c8c9531dc671dcabeaeaefbaa
Gradess <trofimovjava@gmail.com>
2021-10-31 18:21:07 +0300

    Add grid library

---
+++ Source/Survival/Private/Core/WorldGrid/GridLibrary.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "Core/WorldGrid/GridLibrary.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+EGridDirection UGridLibrary::GetGridDirection(const FVector& WorldDirection)
+{
+	const auto Yaw = UKismetMathLibrary::Conv_VectorToRotator(WorldDirection).Yaw;
+
+	if (-45.f < Yaw && Yaw <= 45.f)
+	{
+		return EGridDirection::North;
+	}
+
+	if (45.f < Yaw && Yaw <= 135.f)
+	{
+		return EGridDirection::East;
+	}
+
+	if (135.f < Yaw || Yaw <= -135.f)
+	{
+		return EGridDirection::South;
+	}
+
+	return EGridDirection::West;
+}
+
+EGridDirection UGridLibrary::GetGridDirectionForActor(AActor* ActorToUse)
+{
+	if (!IsValid(ActorToUse)) { return EGridDirection::North; }
+
+	return GetGridDirection(ActorToUse->GetActorForwardVector());
+}
---
+++ Source/Survival/Public/Core/WorldGrid/GridLibrary.h
@@ -0,0 +1,39 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GridDirectionEnum.h"
+#include "UObject/Object.h"
+#include "GridLibrary.generated.h"
+
+UCLASS()
+class SURVIVAL_API UGridLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(
+		BlueprintPure,
+		Category = "GridLibrary",
+		meta = (
+			DisplayName = "ToGridDirection (Vector)",
+			AutoCreateRefTerm = "WorldDirection",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static EGridDirection GetGridDirection(const FVector& WorldDirection);
+
+	UFUNCTION(
+		BlueprintPure,
+		Category = "GridLibrary",
+		meta = (
+			DisplayName = "ToGridDirection (Actor)",
+			AutoCreateRefTerm = "WorldDirection",
+			CompactNodeTitle = "->",
+			BlueprintAutocast
+		)
+	)
+	static EGridDirection GetGridDirectionForActor(AActor* ActorToUse);
+};
