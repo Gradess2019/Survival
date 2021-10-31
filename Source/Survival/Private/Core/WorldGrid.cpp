27775b3bf3aceb8748f768e8e6b59fb8c0f0a08f
Gradess <trofimovjava@gmail.com>
2021-10-31 17:10:24 +0300

    Add simple world grid

---
+++ Source/Survival/Private/Core/WorldGrid.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "Core/WorldGrid.h"
+
+
+AWorldGrid::AWorldGrid()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	CellSize = 100;
+	SizeX = 100;
+	SizeY = 100;
+}
+
+void AWorldGrid::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (bDebug)
+	{
+		SpawnTiles(SizeX, SizeY);
+	}
+}
+
+void AWorldGrid::SpawnTiles(int32 X, int32 Y)
+{
+	if (!IsValid(InstancedStaticMeshComponent))
+	{
+		InstancedStaticMeshComponent = FindComponentByClass<UInstancedStaticMeshComponent>();
+		if (!IsValid(InstancedStaticMeshComponent)) { return; }
+	}
+
+	const int32 HalfX = X / 2;
+	const int32 HalfY = Y / 2;
+	for (int32 CurrentY = -HalfY; CurrentY < HalfY; ++CurrentY)
+	{
+		for (int32 CurrentX = -HalfX; CurrentX < HalfX; ++CurrentX)
+		{
+			auto InstanceLocation = FVector(CurrentX * CellSize, CurrentY * CellSize, 0.f);
+			InstancedStaticMeshComponent->AddInstance(FTransform(InstanceLocation));
+		}
+	}
+}
---
+++ Source/Survival/Public/Core/WorldGrid.h
@@ -0,0 +1,60 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WorldGrid.generated.h"
+
+UCLASS(
+	Blueprintable,
+	BlueprintType
+)
+class SURVIVAL_API AWorldGrid : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWorldGrid();
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void SpawnTiles(int32 X, int32 Y);
+
+protected:
+#pragma region DebugProperties
+	UPROPERTY(
+		EditAnywhere,
+		BlueprintReadOnly,
+		Category = "WorldGrid|Debug"
+	)
+	bool bDebug;
+
+	UPROPERTY
+	(
+		EditAnywhere,
+		BlueprintReadOnly,
+		Category = "WorldGrid|Debug"
+	)
+	int32 CellSize;
+
+	UPROPERTY(
+		EditAnywhere,
+		BlueprintReadOnly,
+		Category = "WorldGrid|Debug"
+	)
+	int32 SizeX;
+
+	UPROPERTY(
+		EditAnywhere,
+		BlueprintReadOnly,
+		Category = "WorldGrid|Debug"
+	)
+	int32 SizeY;
+
+	UPROPERTY()
+	UInstancedStaticMeshComponent* InstancedStaticMeshComponent;
+#pragma endregion DebugProperties
+
+};
