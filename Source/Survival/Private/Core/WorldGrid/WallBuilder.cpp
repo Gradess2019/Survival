cc1ca85484fd613fd472d00be0f6d55ed6460e28
Gradess <trofimovjava@gmail.com>
2021-11-02 00:27:38 +0300

    Add wall builder

---
+++ Source/Survival/Private/Core/WorldGrid/WallBuilder.cpp
@@ -0,0 +1,69 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "Core/WorldGrid/WallBuilder.h"
+
+#include "Core/WorldGrid/Grid.h"
+
+
+AWallBuilder::AWallBuilder()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	MeshComponent = CreateDefaultSubobject<UInstancedStaticMeshComponent>("InstancedStaticMesh");
+	MeshComponent->SetupAttachment(RootComponent);
+}
+
+int32 AWallBuilder::CreateWall(
+	const FVector& Location,
+	const EGridDirection Direction
+)
+{
+	const auto CellLocation = IGrid::Execute_SnapLocation(Grid, Location);
+	const auto NextCellLocation = IGrid::Execute_GetCellLocation(Grid, Location, Direction, 1);
+
+	const auto WallLocation = CellLocation + GetWallOffset(Direction);
+	const auto WallRotation = GetWallRotation(Direction);
+
+	const auto Id = MeshComponent->AddInstanceWorldSpace(FTransform(WallRotation, WallLocation));
+
+	IGrid::Execute_CreateCell(Grid, CellLocation);
+	IGrid::Execute_CreateCell(Grid, NextCellLocation);
+	return Id;
+}
+
+UObject* AWallBuilder::GetWorldGrid() const
+{
+	return Cast<UObject>(Grid);
+}
+
+FVector AWallBuilder::GetWallOffset(EGridDirection Direction) const
+{
+	const auto GridSize = IGrid::Execute_GetGridSize(Grid);
+	const auto HalfGridSize = GridSize / 2;
+	switch (Direction)
+	{
+	case EGridDirection::North: return FVector(HalfGridSize, -HalfGridSize, 0);
+	case EGridDirection::South:
+	case EGridDirection::West: return FVector(-HalfGridSize, -HalfGridSize, 0);
+	case EGridDirection::East: return FVector(-HalfGridSize, HalfGridSize, 0);
+	default: return FVector::ZeroVector;
+	}
+}
+
+FRotator AWallBuilder::GetWallRotation(EGridDirection Direction) const
+{
+	switch (Direction)
+	{
+	case EGridDirection::North: return FRotator(0, 90, 0);
+	case EGridDirection::South: return FRotator(0, 90, 0);
+	default: return FRotator::ZeroRotator;
+	}
+}
+
+void AWallBuilder::SetGrid(UObject* NewGrid)
+{
+	check(NewGrid && NewGrid->Implements<UGrid>())
+	Grid = NewGrid;
+}
---
+++ Source/Survival/Public/Core/WorldGrid/WallBuilder.h
@@ -0,0 +1,76 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GridDirectionEnum.h"
+#include "GameFramework/Actor.h"
+#include "WallBuilder.generated.h"
+
+#pragma region Forward declarations
+class IGrid;
+#pragma endregion Forward declarations
+
+
+UCLASS(
+	BlueprintType,
+	Blueprintable
+)
+class SURVIVAL_API AWallBuilder : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWallBuilder();
+
+protected:
+	UPROPERTY(
+		BlueprintReadOnly,
+		EditDefaultsOnly,
+		Category = "WallBuilder"
+	)
+	UInstancedStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(
+		BlueprintReadOnly,
+		Category = "WallBuilder"
+	)
+	UObject* Grid;
+
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "WallBuilder",
+		meta = (
+			AutoCreateRefTerm = "Location"
+		)
+	)
+	int32 CreateWall(
+		const FVector& Location,
+		const EGridDirection Direction
+	);
+
+	UFUNCTION(
+		BlueprintPure,
+		Category = "WallBuilder"
+	)
+	UObject* GetWorldGrid() const;
+
+	UFUNCTION(
+		BlueprintPure,
+		Category = "WallBuilder"
+	)
+	FVector GetWallOffset(EGridDirection Direction) const;
+
+	UFUNCTION(
+		BlueprintPure,
+		Category = "WallBuilder"
+	)
+	FRotator GetWallRotation(EGridDirection Direction) const;
+
+public:
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "WallBuilder"
+	)
+	void SetGrid(UObject* NewGrid);
+};
