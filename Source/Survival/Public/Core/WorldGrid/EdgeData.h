7a1ef60aa95e334da50f55ff375e826d49fbac98
Gradess <trofimovjava@gmail.com>
2021-11-02 22:13:13 +0300

    Add edge data

---
+++ Source/Survival/Public/Core/WorldGrid/EdgeData.h
@@ -0,0 +1,50 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EdgeData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEdgeData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(
+		EditAnywhere,
+		BlueprintReadWrite,
+		Category = "EdgeData"
+	)
+	const UInstancedStaticMeshComponent* InstancedStaticMeshComponent;
+
+	UPROPERTY(
+		EditAnywhere,
+		BlueprintReadWrite,
+		Category = "EdgeData"
+	)
+	int32 InstanceId;
+
+	FEdgeData(
+		const UInstancedStaticMeshComponent* InstancedStaticMeshComponent = nullptr,
+		int32 InstanceId = -1
+	)
+		: InstancedStaticMeshComponent(InstancedStaticMeshComponent),
+		  InstanceId(InstanceId)
+	{
+	}
+
+	bool operator==(const FEdgeData& Other) const
+	{
+		return Equals(Other);
+	}
+
+	bool Equals(const FEdgeData& Other) const
+	{
+		return InstancedStaticMeshComponent == Other.InstancedStaticMeshComponent && InstanceId == Other.InstanceId;
+	}
+
+	bool IsValid() const
+	{
+		return InstancedStaticMeshComponent != nullptr && InstanceId >= 0;
+	}
+};
