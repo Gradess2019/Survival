fa4949a9f4cae038b43fb5556f5ffcf959624adf
Gradess <trofimovjava@gmail.com>
2021-11-07 06:13:58 +0300

    Fix saving and loading

--- Source/Survival/Private/Core/Serialization/SaveGameArchive.cpp
+++ Source/Survival/Private/Core/Serialization/SaveGameArchive.cpp
@@ -12,45 +12,15 @@ FArchive& FSaveGameArchive::operator<<(UObject*& Object)
 
 		const auto Class = FindObject<UClass>(nullptr, *LoadedString, false);
 		if (!Class) { return *this; }
-		
+
 		Object = NewObject<UObject>(Class, Class);
-
-		for (TFieldIterator<FProperty> It(Object->GetClass()); It; ++It)
-		{
-			FProperty* Property = *It;
-
-			if (!(Property->PropertyFlags & CPF_SaveGame)) { continue; }
-
-			auto Destination = TArray<uint8>();
-			Destination.SetNum(Property->ElementSize);
-			InnerArchive << Destination;
-
-			const auto ValuePtr = Property->ContainerPtrToValuePtr<void>(Object);
-			Property->CopySingleValue(ValuePtr, Destination.GetData());
-		}
-
-		if(!Object && bLoadIfFindFails)
-		{
-			Object = LoadObject<UObject>(nullptr, *LoadedString);
-		}
 	}
 	else
 	{
 		FString SavedClass(Object->GetClass()->GetPathName());
 		InnerArchive << SavedClass;
-		for (TFieldIterator<FProperty> It(Object->GetClass()); It; ++It)
-		{
-			FProperty* Property = *It;
-			if (!(Property->PropertyFlags & CPF_SaveGame)) { continue; }
-			
-			auto Destination = TArray<uint8>();
-			Destination.SetNum(Property->ElementSize);
-
-			const auto ValuePtr = Property->ContainerPtrToValuePtr<void>(Object);
-			Property->CopySingleValue(Destination.GetData(), ValuePtr);
-			
-			InnerArchive << Destination;
-		}
 	}
+
+	Object->Serialize(*this);
 	return *this;
 }
