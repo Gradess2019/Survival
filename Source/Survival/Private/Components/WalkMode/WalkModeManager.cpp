cc25769f6d90c8ec1a825041720243548bd4e54c
Gradess <trofimovjava@gmail.com>
2021-11-13 21:41:17 +0300

    Add walk mode manager component

---
+++ Source/Survival/Private/Components/WalkMode/WalkModeManager.cpp
@@ -0,0 +1,143 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+
+#include "Components/WalkMode/WalkModeManager.h"
+
+#include "AbilitySystem/Tags.h"
+#include "Characters/SurvivalCharacter.h"
+#include "Enums/SurvivalMovementMode.h"
+
+
+UWalkModeManager::UWalkModeManager()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+
+	DefaultMovementMode = ESurvivalMovementMode::Walk;
+	PreviousMovementMode = ESurvivalMovementMode::Idle;
+	CurrentMovementMode = ESurvivalMovementMode::Idle;
+
+	MovementTag = {
+		{ESurvivalMovementMode::Idle, FGameplayTag::RequestGameplayTag(MovementIdleTagName)},
+		{ESurvivalMovementMode::Crouch, FGameplayTag::RequestGameplayTag(MovementCrouchTagName)},
+		{ESurvivalMovementMode::Walk, FGameplayTag::RequestGameplayTag(MovementWalkTagName)},
+		{ESurvivalMovementMode::Run, FGameplayTag::RequestGameplayTag(MovementRunTagName)},
+		{ESurvivalMovementMode::Sprint, FGameplayTag::RequestGameplayTag(MovementSprintTagName)},
+	};
+}
+
+void UWalkModeManager::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	Character = GetOwner<ASurvivalCharacter>();
+	check(Character);
+
+	Character->OnPressMoveKey.AddDynamic(this, &UWalkModeManager::OnPressMoveAction);
+}
+
+void UWalkModeManager::ToggleWalkMode()
+{
+	switch (DefaultMovementMode)
+	{
+	case ESurvivalMovementMode::Walk:
+		{
+			DefaultMovementMode = ESurvivalMovementMode::Run;
+			break;
+		}
+	case ESurvivalMovementMode::Run:
+		{
+			DefaultMovementMode = ESurvivalMovementMode::Walk;
+			break;
+		}
+	default: return;
+	}
+
+	SetMovementModeToDefaultIfMoving();
+}
+
+void UWalkModeManager::ToggleCrouch()
+{
+	switch (DefaultMovementMode)
+	{
+	case ESurvivalMovementMode::Sprint: return;
+	case ESurvivalMovementMode::Crouch:
+		{
+			DefaultMovementMode = PreviousMovementMode;
+			PreviousMovementMode = ESurvivalMovementMode::Crouch;
+			break;
+		}
+	case ESurvivalMovementMode::Idle:
+	case ESurvivalMovementMode::Walk:
+	case ESurvivalMovementMode::Run:
+		{
+			PreviousMovementMode = DefaultMovementMode;
+			DefaultMovementMode = ESurvivalMovementMode::Crouch;
+			break;
+		}
+	default: return;
+	}
+
+	SetMovementModeToDefaultIfMoving();
+}
+
+void UWalkModeManager::ToggleSprint()
+{
+	switch (DefaultMovementMode)
+	{
+	case ESurvivalMovementMode::Sprint:
+		{
+			DefaultMovementMode = PreviousMovementMode;
+			PreviousMovementMode = ESurvivalMovementMode::Run;
+			break;
+		}
+	case ESurvivalMovementMode::Idle:
+	case ESurvivalMovementMode::Crouch:
+	case ESurvivalMovementMode::Walk:
+	case ESurvivalMovementMode::Run:
+		{
+			PreviousMovementMode = DefaultMovementMode;
+			DefaultMovementMode = ESurvivalMovementMode::Sprint;
+			break;
+		}
+	default: return;
+	}
+
+	SetMovementModeToDefaultIfMoving();
+}
+
+ESurvivalMovementMode UWalkModeManager::GetDefaultMovementMode() const
+{
+	return DefaultMovementMode;
+}
+
+ESurvivalMovementMode UWalkModeManager::GetCurrentMovementMode() const
+{
+	return CurrentMovementMode;
+}
+
+void UWalkModeManager::SetMovementMode(ESurvivalMovementMode NewMode)
+{
+	CurrentMovementMode = NewMode;
+	UpdateMovementMode();
+}
+
+void UWalkModeManager::SetMovementModeToDefaultIfMoving()
+{
+	if (CurrentMovementMode == ESurvivalMovementMode::Idle) { return; }
+
+	SetMovementMode(DefaultMovementMode);
+}
+
+void UWalkModeManager::UpdateMovementMode()
+{
+	const auto& Tag = MovementTag[CurrentMovementMode];
+	Character->ActivateAbility(FGameplayTagContainer(Tag));
+}
+
+void UWalkModeManager::OnPressMoveAction(FKey Key)
+{
+	if (CurrentMovementMode != ESurvivalMovementMode::Idle) { return; }
+
+	SetMovementMode(DefaultMovementMode);
+}
---
+++ Source/Survival/Public/Components/WalkMode/WalkModeManager.h
@@ -0,0 +1,90 @@
+﻿// Copyright Gradess. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WalkModeManager.generated.h"
+
+
+#pragma region Forward declarations
+struct FGameplayTag;
+class ASurvivalCharacter;
+enum class ESurvivalMovementMode : uint8;
+#pragma endregion Forward declarations
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SURVIVAL_API UWalkModeManager : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UWalkModeManager();
+	virtual void InitializeComponent() override;
+
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "WalMode"
+	)
+	void ToggleWalkMode();
+
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "WalMode"
+	)
+	void ToggleCrouch();
+
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "WalMode"
+	)
+	void ToggleSprint();
+
+	UFUNCTION(
+		BlueprintGetter,
+		Category = "WalMode"
+	)
+	ESurvivalMovementMode GetDefaultMovementMode() const;
+
+	UFUNCTION(
+		BlueprintGetter,
+		Category = "WalMode"
+	)
+	ESurvivalMovementMode GetCurrentMovementMode() const;
+
+protected:
+	UFUNCTION()
+	void SetMovementMode(ESurvivalMovementMode NewMode);
+
+	UFUNCTION()
+	void SetMovementModeToDefaultIfMoving();
+
+	UFUNCTION()
+	void UpdateMovementMode();
+
+	UFUNCTION()
+	void OnPressMoveAction(FKey Key);
+
+private:
+	UPROPERTY(
+		BlueprintGetter = GetDefaultMovementMode,
+		Category = "WalMode"
+	)
+	ESurvivalMovementMode DefaultMovementMode;
+
+	UPROPERTY()
+	ESurvivalMovementMode PreviousMovementMode;
+
+	UPROPERTY(
+		BlueprintGetter = GetCurrentMovementMode,
+		Category = "WalMode"
+	)
+	ESurvivalMovementMode CurrentMovementMode;
+
+	UPROPERTY()
+	ASurvivalCharacter* Character;
+
+	UPROPERTY(EditAnywhere)
+	TMap<ESurvivalMovementMode, FGameplayTag> MovementTag;
+};
